ee89d9896888e04efcaea046c7adc36aa4ae6d55
Diogo Henrique <diogo.hglima@gmail.com>
2021-05-17 23:11:51 -0300

    adds fetch data from api using miragejs

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,13 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
-export function TransactionsTable(){
+interface Transaction {
+    id: number;
+    title: string;
+    amount: number;
+    type: string;
+    category: string;
+    createdAt: string;
+}
 
+export function TransactionsTable(){
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
     useEffect(() => {
         api.get('transactions')
-            .then(data => console.log(data));
-    }, [])
+            .then(response => setTransactions(response.data.transactions));
+    }, [transactions])
 
     return (
         <Container>
@@ -21,18 +30,15 @@ export function TransactionsTable(){
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Desenvolvimento de website</td>
-                        <td className="deposit">R$12.100</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Aluguel</td>
-                        <td className="withdraw">- R$1.100</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
+                    {transactions.map(transaction => (
+                            <tr key={transaction.id}>
+                                <td>{transaction.title}</td>
+                                <td className={transaction.type}>R${transaction.amount}</td>
+                                <td>{transaction.category}</td>
+                                <td>{transaction.createdAt}</td>
+                            </tr>
+                        ))                       
+                    }    
                 </tbody>
             </table>
         </Container>
